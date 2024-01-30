a343c2bbd642e4d6534a69c32ee43b27224cdb28
stappost <emanuele.scarpantoni@gmail.com>
2024-01-30 14:39:47 +0100

    add function construct

--- index.php
+++ index.php
@@ -5,5 +5,10 @@
         public $genre;
         public $vote;
 
+        function __construct($_title, $_duration, $_genre){
+            $this->title = $_title;
+            $this->duration = $_duration;
+            $this->genre = $_genre;
+        }
     }
 ?>
